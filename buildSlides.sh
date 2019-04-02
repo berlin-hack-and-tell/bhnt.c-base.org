44f89f1f85947fcc56e9da186a5db61d18f7b96e
marlon <kontakt@marlonschultz.de>
2019-04-02 19:37:27 +0200

    added env var

--- buildSlides.sh
+++ buildSlides.sh
@@ -5,6 +5,7 @@ git submodule init
 git pull --recurse-submodules
 
 # run local jekyll server; will be served at localhost:8080
+export JEKYLL_VERSION=3.8
 docker run --rm \
   --volume="$PWD:/srv/jekyll" -p 8080:4000 \
   -it jekyll/jekyll:$JEKYLL_VERSION \
