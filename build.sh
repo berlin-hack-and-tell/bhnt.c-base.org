31f38235c33b898d07b2c0fa1f23cfd0cd5c9efa
Lucas Magasweran <lucasrangit@gmail.com>
2020-05-15 14:33:22 +0200

    build: need to pass local uid/gid for jekyll process to use to avoid permission issues
    This worked before when the Jekyll UID 1000 matched the local user UID. On my current system my UID is not 1000.
    
    ```
    ruby 2.6.3p62 (2019-04-16 revision 67580) [x86_64-linux-musl]
    Configuration file: /srv/jekyll/_config.yml
       GitHub Metadata: site.name is set in _config.yml, but many plugins and themes expect site.title to be used instead. To avoid potential inconsistency, Jekyll GitHub Metadata will not set site.title to the repository's name.
                Source: /srv/jekyll
           Destination: /srv/jekyll/_site
     Incremental build: disabled. Enable with --incremental
          Generating...
       GitHub Metadata: No GitHub API authentication could be found. Some fields may be missing or have incorrect data.
    jekyll 3.7.4 | Error:  Permission denied @ dir_s_mkdir - /srv/jekyll/_site
    ```

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ git pull --recurse-submodules
 export JEKYLL_VERSION=3.8
 docker run --rm \
   --volume="$PWD:/srv/jekyll" -p 4000:4000 \
+  --env JEKYLL_UID=$(id -u) --env JEKYLL_GID=$(id -g) \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve --future --drafts
