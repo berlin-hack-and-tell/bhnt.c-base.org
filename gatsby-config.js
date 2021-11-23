9a383c66faf0c6bc3053be14c0eb25feed4ee74a
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2021-11-23 16:45:59 +0100

    fix: Add missing site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
+    siteMetadata: {
+        title: `Berlin Hack & Tell`,
+        description: `Berlin Hack and Tell is a monthly meetup where eight people present their hacks. You have five minutes followed by up to five minutes of questions. Show us code, not slides or a sales pitch!
+  Follow our [Code of Conduct](https://hackandtell.org/#code_of_conduct) and show off a silly/hobby project that you can't present elsewhere. Think of it like "stand up hacking".
+  Check out the past projects below and sign up on our Meetup page!`,
+        siteUrl: `http://berlinhackandtell.rocks/`,
+        social: {
+            twitter: `berlinhackntell`,
+        },
+    },
     plugins: ["gatsby-transformer-remark"] //via https://www.gatsbyjs.com/blog/2017-11-08-migrate-from-jekyll-to-gatsby/
 }
