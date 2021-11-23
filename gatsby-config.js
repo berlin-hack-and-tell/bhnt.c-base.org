36295f8d243f59d4a79351145e7241edd9fe9874
Stefan Hoth <45467+stefanhoth@users.noreply.github.com>
2021-11-23 17:13:58 +0100

    feat: Add initial version
    Based on gatsby-blog-starter config
    Currently without styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,122 @@ module.exports = {
             twitter: `berlinhackntell`,
         },
     },
-    plugins: ["gatsby-transformer-remark"] //via https://www.gatsbyjs.com/blog/2017-11-08-migrate-from-jekyll-to-gatsby/
+    plugins: [
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/events`,
+                name: `blog`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/content/images`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 630,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    `gatsby-remark-prismjs`,
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        // {
+        //   resolve: `gatsby-plugin-google-analytics`,
+        //   options: {
+        //     trackingId: `ADD YOUR TRACKING ID HERE`,
+        //   },
+        // },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.nodes.map(node => {
+                                return Object.assign({}, node.frontmatter, {
+                                    description: node.excerpt,
+                                    date: node.frontmatter.date,
+                                    url: site.siteMetadata.siteUrl + node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + node.fields.slug,
+                                    custom_elements: [{ "content:encoded": node.html }],
+                                })
+                            })
+                        },
+                        query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+                        output: "/rss.xml",
+                        title: "Gatsby Starter Blog RSS Feed",
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Gatsby Starter Blog`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                // This will impact how browsers show your PWA/website
+                // https://css-tricks.com/meta-theme-color-and-trickery/
+                // theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ]
 }
