05baac890ff08c0a91f41048a4388378221f164a
ligi <ligi@ligi.de>
2023-07-24 23:42:04 +0200

    Extend copy announcement script to create slides file (#364)

--- copy-announcement.sh
+++ copy-announcement.sh
@@ -12,7 +12,9 @@ name=$(echo "${event_json}" | sed -n 's/{[^{]*"name":"\([^"]*\)".*$/\1/p')
 bhnt_no=$(echo "${name}" | sed -n 's/^[^#]*#\([0-9]\+\).*$/\1/p')
 bhnt_title=$(echo "${name}" | sed -n 's/^\(.*[^ ]\) * - BHNT .*$/\1/p')
 file_title=$(echo "${bhnt_title}" | tr A-Z a-z | sed 's/[^0-9a-z]/-/g')
-post_file="_posts/${local_date}-no${bhnt_no}-${file_title}.md"
+file_name="${local_date}-no${bhnt_no}-${file_title}.md"
+post_file="_posts/${file_name}"
+slides_file="_slides/${file_name}"
 
 echo
 echo "Generating ${post_file} ..."
@@ -27,6 +29,21 @@ published: true
 time: '${local_time}'
 location: '[c-base](https://www.c-base.org)'
 meetupUrl: https://www.meetup.com/de-DE/berlin-hack-and-tell/events/${event_id}
+---
+
+Upcoming
+EOF
+echo '#############################################################################'
+echo "Generating ${slides_file} ..."
+echo '#############################################################################'
+
+tee "${slides_file}" <<EOF
+---
+layout: slides
+title: Berlin Hack & Tell \\#${bhnt_no} - ${bhnt_title}
+date: ${local_date}
+time: '${local_time}'
+description: Slides for BHNT \\#${bhnt_no}
 ---
 
 Upcoming
