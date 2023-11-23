c8e3edd93b4600a028ec78a3992c198a5211af66
ligi <ligi@ligi.de>
2023-11-23 16:47:21 +0300

    Improve copy announcement (#391)
    so it now includes creating the slide scaffold

--- copy-announcement.sh
+++ copy-announcement.sh
@@ -46,7 +46,126 @@ time: '${local_time}'
 description: Slides for BHNT \\#${bhnt_no}
 ---
 
-Upcoming
+
+<section data-markdown>
+## BHNT \\#${bhnt_no} - ${bhnt_title}
+
+![](/assets/images/088/camp.jpg)
+
+We start 1923 *sharpIsh*
+</section>
+
+<section>
+<h2>Structure</h2>
+
+<ol start="0">
+  <li>Intro (this)</li>
+  <li>Rules</li>
+  <li>News</li>
+  <li>Slots 0..3</li>
+  <li>Open Mic</li>
+  <li>Break</li>
+  <li>Slots 4..7</li>
+  <li>Vote/Trophy</li>
+</ol>
+
+</section>
+
+<section>
+<h2>Rules</h2>
+
+<ol start="0">
+  <li>Be excellent to each other</li>
+  <li>Do not smoke</li>
+  <li>Respect the 5min</li>
+  <li>Should be your thing</li>
+  <li>NO PURE SLIDEDECKS</li>
+  <li>No pitches</li>
+  <li>No business</li>
+  <li>Entertain us!</li>
+</ol>
+</section>
+
+<section data-markdown>
+## &#9812;
+<center>
+KEEP CALM</br>
+AND</br>
+KEEP YOUR</br>
+PHONES IN</br>
+SILENT MODE</br>
+</center>
+</section>
+
+<section>
+<h2><a href="https://berlinhackandtell.rocks/">berlinhackandtell.rocks</a></h2>
+<iframe class="stretch" data-src="https://berlinhackandtell.rocks"></iframe>
+</section>
+
+<section>
+<pre>https://pad.systemli.org/p/bhnt0${bhnt_no}-keep</pre>
+<iframe name="embed_readwrite" src="https://pad.systemli.org/p/bhnt0${bhnt_no}?showControls=false&showChat=false&showLineNumbers=true&useMonospaceFont=true" width="100%" height="600" frameborder="0" class="stretch"></iframe>
+</section>
+
+<section data-markdown>
+![Hack and Tell Trophy]({{ '/assets/images/trophy.jpg' | relative_url }})
+</section>
+
+<section data-markdown>
+![Hack and Tell Trophies]({{ '/assets/images/trophies.jpg' | relative_url }})
+</section>
+
+<section data-markdown>
+## WarmUp
+</section>
+
+
+<section>
+<center>
+<img src="/assets/images/090/infusion.png"/>
+</center>
+</section>
+
+
+<section data-markdown>
+## Slots 0..3
+</section>
+
+<section data-markdown>
+## Open Mic
+
+* Line up at the stage
+* Stick to 1-2 sentences
+</section>
+
+<section>
+<h2>Break</h2>
+
+<ul>
+<li>Until <input style="margin-left: 0.2em; font-size: 100%; width: 4em; border: 1px solid white; background-color: transparent; color: white; text-align: center;"></li>
+<li>Bottles back to the bar</li>
+<li>Update your presentation details:</li>
+</ul>
+<center>
+<img src="http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data=http%3A%2F%2Fpad.systemli.org%2Fp%2Fbhnt0&amp;qzone=1&amp;margin=0&amp;size=300x300&amp;ecc=L" alt="QR code">
+<div style="font-family: mono; font-size: 70%;">https://pad.systemli.org/p/bhnt0${bhnt_no}</div>
+</center>
+</section>
+
+<section>
+<h2>Vote</h2>
+
+<ul>
+<li>wifi:c-base-public</li>
+<li>pw:hacktheplanet</li>
+<li>http://172.31.78.169</li>
+</ul>
+<center>
+<img src="http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data=http%3A%2F%2F172.31.78.169&amp;qzone=1&amp;margin=0&amp;size=400x400&amp;ecc=L" alt="QR code">
+</center>
+</section>
+
+
 EOF
 echo '#############################################################################'
 
