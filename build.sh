183a1ca75d0cbd9483bde8f217ff035916c3c633
Lucas Magasweran <lucasrangit@gmail.com>
2026-04-21 13:13:09 +0200

    build: add full build option to document solution when there are missing posts
    Signed-off-by: Lucas Magasweran <lucasrangit@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ set -o xtrace
 git submodule init
 git pull --recurse-submodules
 
+JEKYLL_COMMAND="jekyll serve --host 0.0.0.0 --future --drafts --watch --force_polling"
+# if 'full' is passed, don't add '--incremental'` flag
+# use this if you encounter rendering issues (e.g. missing posts on the homepage)
+if [ "$1" != "full" ]; then
+  JEKYLL_COMMAND="$JEKYLL_COMMAND --incremental"
+fi
+
 # run local jekyll server; will be served at http://localhost:4000
 # latest version from https://github.com/github/pages-gem/releases
 export GITHUB_PAGES_GEM_VERSION=v232
@@ -12,4 +19,4 @@ docker run --rm \
   --volume="$PWD:/src/site" -p 4000:4000 \
   --env JEKYLL_UID=$(id -u) --env JEKYLL_GID=$(id -g) \
   -it ghcr.io/github/pages-gem:$GITHUB_PAGES_GEM_VERSION \
-  jekyll serve --host 0.0.0.0 --future --drafts --watch --force_polling --incremental
+  sh -c "$JEKYLL_COMMAND"
